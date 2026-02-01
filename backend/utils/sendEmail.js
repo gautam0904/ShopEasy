b897fbcc7a303d07c9973fdab2cd7200b4663b7c
Malaviya Gautam <malaviyagautam0942@gmail.com>
2026-02-02 00:45:18 +0530

    a

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -3,21 +3,19 @@ export const sendEmail=async(options)=>{
     const transporter=nodeMailer.createTransport({
         host: process.env.SMTP_HOST,
         port: process.env.SMTP_PORT,
-        secure: false, // false for 587 (STARTTLS)
+        secure: false, 
         service:process.env.SMTP_SERVICE,
         auth:{
             user:process.env.SMTP_MAIL,
             pass:process.env.SMTP_PASSWORD
         },
-        // Optimizations for cloud environments
         tls: {
             rejectUnauthorized: false
         },
-        connectionTimeout: 10000, // 10 seconds
-        greetingTimeout: 10000, 
+        connectionTimeout: 10000, 
         socketTimeout: 10000,
-        debug: true, // Enable debug logs
-        logger: true // Log to console
+        debug: true, 
+        logger: true 
     })
     const mailOptions={
         from:process.env.SMTP_MAIL,
