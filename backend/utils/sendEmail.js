6fde0f5c356e3385f7d877307d9f3bf3d5077dbe
Malaviya Gautam <malaviyagautam0942@gmail.com>
2026-02-02 00:44:20 +0530

    vh

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -8,7 +8,16 @@ export const sendEmail=async(options)=>{
         auth:{
             user:process.env.SMTP_MAIL,
             pass:process.env.SMTP_PASSWORD
-        }
+        },
+        // Optimizations for cloud environments
+        tls: {
+            rejectUnauthorized: false
+        },
+        connectionTimeout: 10000, // 10 seconds
+        greetingTimeout: 10000, 
+        socketTimeout: 10000,
+        debug: true, // Enable debug logs
+        logger: true // Log to console
     })
     const mailOptions={
         from:process.env.SMTP_MAIL,
